cc2a2527f9e8f4a7fcbd5418e805e7db45f12e6a
erdincaltay <63107448+erdincaltay@users.noreply.github.com>
2020-05-07 04:36:29 +0300

    Nmap Final Teslim

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xmldoc = new XmlDocument();
+            string dosya = @"nmap.xml";
+            xmldoc.Load(dosya);
+            xmldoc.Save(Console.Out);
+            Console.ReadKey();
+        }
+    }
+}
